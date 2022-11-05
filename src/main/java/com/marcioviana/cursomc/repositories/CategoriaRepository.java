aa2ee399c2623aebb7eadb52e0654e8476432511
Márcio Viana <marcioferreira_viana@homail.com>
2022-11-04 21:55:45 -0400

    Criando repository e service para Categoria

---
+++ src/main/java/com/marcioviana/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.marcioviana.cursomc.repositories;
+
+import com.marcioviana.cursomc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria,Integer> {
+
+
+
+
+}
