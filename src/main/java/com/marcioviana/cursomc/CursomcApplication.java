e10d678ca06d5cd19c83984c5704258f7f54ce6a
Márcio Viana <marcioferreira_viana@homail.com>
2022-11-06 10:55:27 -0400

    Criando operação de intaciação

--- src/main/java/com/marcioviana/cursomc/CursomcApplication.java
+++ src/main/java/com/marcioviana/cursomc/CursomcApplication.java
@@ -1,14 +1,31 @@
 package com.marcioviana.cursomc;
 
+import com.marcioviana.cursomc.domain.Categoria;
+import com.marcioviana.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+	@Autowired
+private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void  run(String... args) throws  Exception{
+
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new  Categoria(null, "Escritorio");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+
+	}
 }
